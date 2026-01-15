6f51c704a3a87583dba2b533d50e8a084103b892
T-017 <jholt2@byupathway.edu>
2026-01-14 22:58:54 -0600

    js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,17 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("_______");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+if (input.value.trim() !== "") {
+  const input = document.querySelector("#favchap");
+  const button = document.querySelector("button");
+  const list = document.querySelector("_______");
+  const li = document.createElement("li");
+  const deleteButton = document.createElement("button");
 
-li.textContent = input.value;
-deleteButton.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
+  li.textContent = input.value;
+  deleteButton.textContent = "❌";
+  li.append(deleteButton);
+  list.append(li);
+}
+
+button.addEventListener("click", function () {
+  list.removeChild(li);
+  input.focus();
+});
