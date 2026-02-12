c762f5c873b88c2b4e6c23cd4e45ec527d08da01
T-017 <jholt2@byupathway.edu>
2026-02-11 20:01:33 -0600

    subs

--- scripts/form.js
+++ scripts/form.js
@@ -45,14 +45,23 @@ function createFormName(formReview) {
   });
 }
 
-function recordSubmission() {
-  let count = localStorage.getItem('submissionCount');
+// 1️⃣ Initialize display element variable
+const subsDisplay = document.querySelector(".submissions");
 
-  count = count ? parseInt(count, 10) : 0;
+// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numSubs = Number(window.localStorage.getItem("numSubs-ls")) || 0;
 
-  count += 1;
-
-  localStorage.setItem('submissionCount', count);
-
-  console.log(`Submissions so far: ${count}`);
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numSubs !== 0) {
+  subsDisplay.textContent = numSubss;
+} else {
+  subsDisplay.textContent = `Thank you for your Review`;
 }
+
+// 4️⃣ increment the number of visits by one.
+numSubs++;
+
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numSubs-ls", numSubs);
+
+// 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
