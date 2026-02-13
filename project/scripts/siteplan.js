0c207ab6e546123fa6f387c351c9c54a282f3db7
T-017 <jholt2@byupathway.edu>
2026-02-13 16:57:54 -0600

    project

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,2 +1,41 @@
 document.getElementById("currentYear").innerHTML = document.currentYear = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+
+
+// Request Form ------------------------------------------------------------------------------
+const products = [
+  {name: "Flux Capacitor"},
+  {name: "Low Voltage Reactor"},
+  {name: "Power Laces"},
+  {name: "Time Circuits"},
+  {name: "Warp Equalizer"}
+];
+
+function createFormName(formReview) {
+  const selectProd = document.querySelector("#product")
+  formReview.forEach(products => {
+    let name = document.createElement("option");
+
+    name.textContent = products.name;
+    name.value = products.id
+
+    selectProd.appendChild(name);
+  });
+}
+
+createFormName(products);
+
+const subsDisplay = document.querySelector(".submissions");
+
+let numSubs = Number(window.localStorage.getItem(subsDisplay)) || 0;
+numSubs++;
+console.log(numSubs)
+
+if (numSubs !== 0) {
+  subsDisplay.textContent = numSubs;
+} else {
+  subsDisplay.textContent = `This was your first Review. Thank you for your feedback.`;
+}
+
+localStorage.setItem(subsDisplay, numSubs);
