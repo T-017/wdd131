d744d1bed2c828ff2b0ba4f994005266a609ea65
T-017 <jholt2@byupathway.edu>
2026-02-18 01:06:09 -0600

    travel

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -16,34 +16,34 @@ menuButton.addEventListener("click", () => {
 
 // Request Form ------------------------------------------------------------------------------
 const services = [
-  { name: "Accessibility Services" },
-  { name: "Budget Planning" },
-  { name: "City Selections" },
-  { name: "Cultural Etiquette" },
-  { name: "Emergency Services" },
-  { name: "Festival Schedule" },
-  { name: "Hotel Booking" },
-  { name: "Itinerary Planning" },
-  { name: "Transportation" },
-  { name: "Translation Services" },
-  { name: "Travel Agencies" },
-  { name: "Travel Insurance" },
-  { name: "General Questions"}
+  { assist: "Accessibility Services" },
+  { assist: "Budget Planning" },
+  { assist: "City Selections" },
+  { assist: "Cultural Etiquette" },
+  { assist: "Emergency Services" },
+  { assist: "Festival Schedule" },
+  { assist: "Hotel Booking" },
+  { assist: "Itinerary Planning" },
+  { assist: "Transportation" },
+  { assist: "Translation Services" },
+  { assist: "Travel Agencies" },
+  { assist: "Travel Insurance" },
+  { assist: "General Questions"}
 ];
 
-function createFormName(formRequest) {
+function createFormAssist(formRequest) {
   const selectProd = document.querySelector("#request")
   formRequest.forEach(services => {
-    let name = document.createElement("option");
+    let assist = document.createElement("option");
 
-    name.textContent = services.name;
-    name.value = services.id
+    assist.textContent = services.assist;
+    assist.value = services.id
 
-    selectProd.appendChild(name);
+    selectProd.appendChild(assist);
   });
 }
 
-createFormName(services);
+createFormAssist(services);
 
 const subsDisplay = document.querySelector(".submissions");
 
@@ -58,3 +58,57 @@ if (numSubs !== 0) {
 }
 
 localStorage.setItem(subsDisplay, numSubs);
+
+// Travel Assistance -------------------------------------------------------------------------
+const temples = [
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  }
+];
+
+createTempleCard(temples);
+
+function createTempleCard(filteredTemples) {
+  document.querySelector(".image-section").innerHTML = "";
+  filteredTemples.forEach(temple => {
+    let card = document.createElement("section");
+    let name = document.createElement("h2");
+    let location = document.createElement("p");
+    let dedicated = document.createElement("p");
+    let area = document.createElement("p");
+    let img = document.createElement("img");
+    let url = document.createElement("a");
+
+    name.textContent = temple.templeName;
+    location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+    dedicated.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+    area.innerHTML = `<span class="label">Area:</span> ${temple.area} sq ft`;
+    img.setAttribute("src", temple.imageUrl);
+    img.setAttribute("alt", `${temple.templeName} Temple`);
+    img.setAttribute("loading", "lazy");
+    url.setAttribute("href", temple.URL);
+    url.textContent = "More Information";
+
+    card.appendChild(name);
+    card.appendChild(location);
+    card.appendChild(dedicated);
+    card.appendChild(area);
+    card.appendChild(img);
+    card.appendChild(url);
+
+    document.querySelector(".image-section").appendChild(card);
+  });
+}
