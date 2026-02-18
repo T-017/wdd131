8084e4974dc29558b67e92c257d222c21764dda9
T-017 <jholt2@byupathway.edu>
2026-02-18 01:23:31 -0600

    travel

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -76,13 +76,78 @@ const temples = [
     area: 74792,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+  },
+  {
+    templeName: "Tokyo Japan",
+    location: "Tokyo, Japan",
+    dedicated: "1980, October, 27",
+    area: 53997,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/tokyo-japan-temple/tokyo-japan-temple-26340-main.jpg"
+  },
+  {
+    templeName: "Sapporo Japan",
+    location: "Sapporo, Japan",
+    dedicated: "2016, August, 21",
+    area: 48480,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/sapporo-japan-temple/sapporo-japan-temple-3374-main.jpg"
+  },
+  {
+    templeName: "Fukuoka Japan",
+    location: "Fukuoka, Japan",
+    dedicated: "2000, June, 11",
+    area: 10700,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/fukuoka-japan-temple/fukuoka-japan-temple-14618-main.jpg",
+    URL: "https://www.japan.travel/en/my/travel-trade/jp-travel-agencies-list/"
   }
 ];
 
 createTempleCard(temples);
 
 function createTempleCard(filteredTemples) {
-  document.querySelector(".image-section").innerHTML = "";
+  document.querySelector(".traveling-assist").innerHTML = "";
   filteredTemples.forEach(temple => {
     let card = document.createElement("section");
     let name = document.createElement("h2");
@@ -91,7 +156,6 @@ function createTempleCard(filteredTemples) {
     let area = document.createElement("p");
     let img = document.createElement("img");
     let url = document.createElement("a");
-
     name.textContent = temple.templeName;
     location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
     dedicated.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
@@ -109,6 +173,6 @@ function createTempleCard(filteredTemples) {
     card.appendChild(img);
     card.appendChild(url);
 
-    document.querySelector(".image-section").appendChild(card);
+    document.querySelector(".traveling-assist").appendChild(card);
   });
 }
