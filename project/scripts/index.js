6274b0dc9315a0387ebb6389fb104e3c00220629
T-017 <jholt2@byupathway.edu>
2026-02-18 01:25:54 -0600

    travel

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -14,51 +14,6 @@ menuButton.addEventListener("click", () => {
   }
 });
 
-// Request Form ------------------------------------------------------------------------------
-const services = [
-  { assist: "Accessibility Services" },
-  { assist: "Budget Planning" },
-  { assist: "City Selections" },
-  { assist: "Cultural Etiquette" },
-  { assist: "Emergency Services" },
-  { assist: "Festival Schedule" },
-  { assist: "Hotel Booking" },
-  { assist: "Itinerary Planning" },
-  { assist: "Transportation" },
-  { assist: "Translation Services" },
-  { assist: "Travel Agencies" },
-  { assist: "Travel Insurance" },
-  { assist: "General Questions" }
-];
-
-function createFormAssist(formRequest) {
-  const selectProd = document.querySelector("#request")
-  formRequest.forEach(services => {
-    let assist = document.createElement("option");
-
-    assist.textContent = services.assist;
-    assist.value = services.id
-
-    selectProd.appendChild(assist);
-  });
-}
-
-createFormAssist(services);
-
-const subsDisplay = document.querySelector(".submissions");
-
-let numSubs = Number(window.localStorage.getItem(subsDisplay)) || 0;
-numSubs++;
-console.log(numSubs)
-
-if (numSubs !== 0) {
-  subsDisplay.textContent = numSubs;
-} else {
-  subsDisplay.textContent = `This was your first request. Thank you and we will get back to you soon.`;
-}
-
-localStorage.setItem(subsDisplay, numSubs);
-
 // Travel Assistance -------------------------------------------------------------------------
 const temples = [
   {
@@ -176,3 +131,48 @@ function createTempleCard(filteredTemples) {
     document.querySelector(".traveling-assist").appendChild(card);
   });
 }
+
+// Request Form ------------------------------------------------------------------------------
+const services = [
+  { assist: "Accessibility Services" },
+  { assist: "Budget Planning" },
+  { assist: "City Selections" },
+  { assist: "Cultural Etiquette" },
+  { assist: "Emergency Services" },
+  { assist: "Festival Schedule" },
+  { assist: "Hotel Booking" },
+  { assist: "Itinerary Planning" },
+  { assist: "Transportation" },
+  { assist: "Translation Services" },
+  { assist: "Travel Agencies" },
+  { assist: "Travel Insurance" },
+  { assist: "General Questions" }
+];
+
+function createFormAssist(formRequest) {
+  const selectProd = document.querySelector("#request")
+  formRequest.forEach(services => {
+    let assist = document.createElement("option");
+
+    assist.textContent = services.assist;
+    assist.value = services.id
+
+    selectProd.appendChild(assist);
+  });
+}
+
+createFormAssist(services);
+
+const subsDisplay = document.querySelector(".submissions");
+
+let numSubs = Number(window.localStorage.getItem(subsDisplay)) || 0;
+numSubs++;
+console.log(numSubs)
+
+if (numSubs !== 0) {
+  subsDisplay.textContent = numSubs;
+} else {
+  subsDisplay.textContent = `This was your first request. Thank you and we will get back to you soon.`;
+}
+
+localStorage.setItem(subsDisplay, numSubs);
